7ceaaa4cdb5746874e1e6574b206dc1389c042c9
Calista Ifenkwe <calistaifenkwe@gmail.com>
2023-04-13 18:54:31 +0100

    initial commit

---
+++ Treasure Island/treasure_island.py
@@ -0,0 +1,6 @@
+print("Welcome to the Treasure Island.")
+print("Your mission is to find the treasure.")
+choice1 = input("You're at a crossroad, where do you want to go? Type 'left' or 'right' \n").lower()
+
+if choice1 == "right":
+    print("You fell into a hole. Game Over")
